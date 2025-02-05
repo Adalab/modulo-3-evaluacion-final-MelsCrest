d0622938a306c87a3dfaf930843f71ece2b40ba1
MelsCrest <melisacresta@gmail.com>
2025-02-05 09:37:36 +0100

    imagen por defecto añadida

--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -2,9 +2,12 @@ import PropTypes from "prop-types";
 import '../styles/CharacterCard.scss'
 
 function CharacterCard({info}) {
+
+  const img = info.img !== '' ? info.img : 'https://i.pinimg.com/736x/29/63/88/296388832b6df6e3396a727e791e51cf.jpg'
+
   return (
     <article>
-      <img src={info.img} alt="foto de personaje de Harry Potter" className="card_img"/> {/* recordar hacer la img por defecto https://i.pinimg.com/736x/29/63/88/296388832b6df6e3396a727e791e51cf.jpg */}
+      <img src={img} alt="foto de personaje de Harry Potter" className="card_img"/> 
       <h3>{info.name}</h3>
       <p>{info.species}</p>
     </article>
